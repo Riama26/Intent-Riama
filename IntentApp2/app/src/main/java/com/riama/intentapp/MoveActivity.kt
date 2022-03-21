b5789736a316baf1df0a677d5c219fdd9ff32aff
Riama <101964117+Riama26@users.noreply.github.com>
2022-03-21 15:13:04 +0700

    Add files via upload

---
+++ IntentApp2/app/src/main/java/com/riama/intentapp/MoveActivity.kt
@@ -0,0 +1,12 @@
+package com.riama.intentapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.riama.intentapp.R
+
+class MoveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move)
+    }
+}
